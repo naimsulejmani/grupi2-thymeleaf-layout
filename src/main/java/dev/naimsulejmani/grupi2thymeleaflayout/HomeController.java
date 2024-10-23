a7387e91c72dc772c57b9e9b081c65fb1896d4cf
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-10-23 12:30:50 +0200

    added some tasks and layout file and combined

--- src/main/java/dev/naimsulejmani/grupi2thymeleaflayout/HomeController.java
+++ src/main/java/dev/naimsulejmani/grupi2thymeleaflayout/HomeController.java
@@ -1,4 +1,72 @@
 package dev.naimsulejmani.grupi2thymeleaflayout;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Controller
 public class HomeController {
+    private LocalDateTime startedDate = LocalDateTime.now();
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "Home Page");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("pageTitle", "About Page");
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("pageTitle", "Contact Page");
+        return "contact";
+    }
+
+
+    @GetMapping("/random")
+    public String random(Model model) {
+        model.addAttribute("pageTitle", "Random Page");
+        double random = Math.random();
+        if (random < 0.334) {
+            return "index";
+        } else if (random < 0.667) {
+            return "about";
+        } else {
+            return "contact";
+        }
+    }
+
+    // time page
+    @GetMapping("/time")
+    public String time(Model model) {
+        model.addAttribute("pageTitle", "Time Page");
+        model.addAttribute("serverStarted", startedDate);
+        model.addAttribute("currentTime", LocalDateTime.now());
+        return "time";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/dev/naimsulejmani/grupi2thymeleaflayout/Grupi2ThymeleafLayoutApplicationTests.java
+++ src/test/java/dev/naimsulejmani/grupi2thymeleaflayout/Grupi2ThymeleafLayoutApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Grupi2ThymeleafLayoutApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
